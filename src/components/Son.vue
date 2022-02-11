cb37c3044c582e36bbde498ade94207d394e57f2
PINK <>
2022-02-11 11:35:25 +0800

    no message

--- src/components/Son.vue
+++ src/components/Son.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <span>我是子组件:{{sonVal}}</span>
-    <button style="width: 150px" @click="changeSon">点我改变子组件和父组件</button>
+    <span style="margin-left: 10px">我是父组件传过来的值:{{isFather}}</span>
+    <el-button style="margin-left: 10px"  @click="changeSon">点我通过$emit改变我自己并传递给父组件</el-button>
+    <el-button  @click="parentUse">点我通过$parent获取父组件的值</el-button>
+    <el-button  @click="parentUse2">点我通过$parent改变父组件的值</el-button>
   </div>
 </template>
 
@@ -24,8 +27,15 @@ export default {
     changeSon(){
       this.sonVal = 500
       this.$emit('changeFather',this.sonVal)
+    },
+    parentUse(){
+      alert(this.$parent.father)
+    },
+    parentUse2(){
+    this.$parent.changeVal()
     }
-  }
+  },
+  props:['isFather']
 };
 </script>
 
--- src/views/componentValueTransfer.vue
+++ src/views/componentValueTransfer.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <div style="display: flex;flex-direction: column">
-      <span>我是夫组件:{{father}}</span>
-      <button style="width: 150px" @click="changeSon">点我改变子组件</button>
+    <div>
+      <span>我是父组件:{{father}}</span>
+      <el-button style="margin-left: 10px" @click="changeVal">点我改变自己</el-button>
+      <el-button @click="changeSon">通过$refs改变子组件</el-button>
     </div>
-    <Son @changeFather="changeVal" ref="son"></Son>
+    <Son style="margin-top: 50px" :is-father="father" @changeFather="sonChangeVal" ref="son"></Son>
+    <explain :content="content"></explain>
   </div>
 
 </template>
@@ -12,19 +14,29 @@
 <script>
 import Grandson from "../components/Grandson";
 import Son from "../components/Son";
+import Explain from "../components/Explain";
 export default {
   name: "componentValueTransfer",
-  components: { Son, Grandson },
+  components: { Explain, Son, Grandson },
   data() {
     return {
+      content: [
+        "在子组件中通过props 绑定父组件传过来的值 ",
+        "父组件可以通过$refs.子组件的方法名调用子组件内的方法",
+        "子组件可以通过$emit（方法名，参数）将数据传递给父组件，需要在父组件中的子组件中绑定子组件定义的方法名监听传递过来的数据",
+        "子组件可以通过$parent.参数或者方法名直接获父组件的变量或调用父组件的方法"
+      ],
       father:30,
     };
   },
   methods:{
+    changeVal(){
+      this.father = 50
+    },
     changeSon(){
       this.$refs.son.changeVal()
     },
-    changeVal(val){
+    sonChangeVal(val){
       console.log(val);
       this.father = val
     },
