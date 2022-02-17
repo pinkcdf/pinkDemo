1d7413e91f6c2cbe2668f9b691333716c4733b64
PINK <>
2022-02-17 17:10:54 +0800

    no message

--- src/views/pixi.vue
+++ src/views/pixi.vue
@@ -5,25 +5,56 @@
 </template>
 
 <script>
-import * as PIXI from 'pixi.js'
+import * as PIXI from "pixi.js";
+
 export default {
   name: "pixi",
+  data() {
+    return {
+      pixi: null,
+      pixiLoader: null,
+    };
+  },
+  mounted() {
+    this.pixi = new PIXI.Application({
+      width: 1000,
+      height: 600,
+      backgroundColor: 0xEF6644
+    });
+    this.initState();
+  },
   methods: {
-    initPixi() {
-      let app = new PIXI.Application({ width:1000,height:600,backgroundColor: 0xEF6644 });
-      document.getElementById("pixi").appendChild(app.view);
+    initState() {
+      document.getElementById("pixi").appendChild(this.pixi.view);
+      this.pixi.loader.add({name:'bunny',url:require("@/assets/img/business-3d-girl-with-phone-1.png")}).load((loader, resources) => {
+        // This creates a texture from a 'bunny.png' image
+        const bunny = new PIXI.Sprite(resources.bunny.texture);
 
-    }
-  },
-  mounted: function() {
-    this.initPixi();
-  },
+        // Setup the position of the bunny
+        bunny.x = 0;
+        bunny.y = 0;
 
+        // Rotate around the center
+        // bunny.anchor.x = 0.5;
+        // bunny.anchor.y = 0.5;
+
+        // Add the bunny to the scene we are building
+        this.pixi.stage.addChild(bunny);
+
+        this.pixi.interactive = true
+
+        // this.pixi.ticker.add(() => {
+        //   bunny.rotation += 0.01;
+        // });
+      });
+
+    },
+  }
 };
 </script>
 
 <style scoped>
-#pixi{
-  border: saddlebrown solid 1px;
+#pixi {
+  /*border: saddlebrown solid 1px;*/
 }
 </style>
